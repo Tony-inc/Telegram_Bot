10442764bb49dc45ca2b1c324a6568de893cbffc
Anton <88192329+Tony-inc@users.noreply.github.com>
2022-04-28 15:42:00 +0300

    Update api.py

--- api.py
+++ api.py
@@ -2,15 +2,15 @@
 import requests
 class Giphy:
     def get_url(self, search_word):
-        api = "UCf57i74uVQ9iOUiXX73F3RJVcmfk8tZ"
+        api = "api_key"
         gif = requests.get(url="https://api.giphy.com/v1/gifs/translate", params={"api_key": api, "s":search_word})
         gif_url = gif.json()["data"]["images"]["original"]["mp4"]
         return gif_url
 
     def get_joke(self):
-        joke = requests.get(url="https://dad-jokes.p.rapidapi.com/random/joke",
+        joke = requests.get(url="end_point",
                             headers={"X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
-                                     "X-RapidAPI-Key": "620bd1d7demsh48804bacb855cecp188b4ejsnfb123ef1a98d"})
+                                     "X-RapidAPI-Key": "api_key"})
         setup = joke.json()["body"][0]["setup"]
         punchline = joke.json()["body"][0]["punchline"]
         return (setup, punchline)
